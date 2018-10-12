412c2ec8150a2a065e53f0d591e03670ab967ba1
JUAN TAKO <juan.tako.dev@gmail.com>
2018-10-12 17:10:16 +0200

    partie 2

--- news.js
+++ news.js
@@ -23,14 +23,39 @@ function selectionner_recherche(e)
 { 
     recherche_courante = $(e).parent().find("label").text();
     $("#zone_saisie").val(recherche_courante);
+
+    var cookie = $.cookie(recherche_courante);
+    if(cookie){
+        var parsed = JSON.parse(cookie);
+        for(var i = 0; i < cookie.length; i++){
+            recherche_courante_news.push(parsed[i]);
+            afficher_resultat(parsed[i], true);
+        }
+    }
+
     rechercher_nouvelles();
 }
 
+function afficher_resultat(res, saved){
+    var str;
+    if(saved)
+        str = "</span><span class=\"action_news\" onclick=\"supprimer_nouvelle(this)\"><img src=\"disk15.jpg\"/></span></p>";
+    else
+        str = "</span><span class=\"action_news\" onclick=\"ajouter_nouvelle(this)\"><img src=\"horloge15.jpg\"/></span></p>"
 
+    $("#resultats").append("<p class=\"titre_result\"><a class=\"titre_news\" href=\""
+    + decodeEntities(parsed[i]["url"])
+    + "\" target=\"_blank\">"
+    + decodeEntities(parsed[i]["titre"])
+    + "</a><span class=\"date_news\">"
+    + format(decodeEntities(parsed[i]["date"]))
+    + str);
+}
 function init()
 {
-    var cookie = JSON.parse($.cookie("recherches"));
+    var cookie = $.cookie("recherches");
     if(cookie){
+        cookie = JSON.parse(cookie);
         for(var i = 0; i < cookie.length; i++){
             recherches.push(cookie[i]);
             $("#recherches-stockees").append("<p class=\"titre-recherche\"><label onclick=\"selectionner_recherche(this)\">" + cookie[i] + "</label><img src=\"croix30.jpg\" class=\"icone-croix\" onclick=\"supprimer_recherche(this)\"/> </p>");
@@ -55,6 +80,10 @@ function rechercher_nouvelles()
 {
     $("#resultats").empty();
     recherche_courante = $("#zone_saisie").val();
+    var cookie = $.cookie(recherche_courante);
+    if(cookie){
+        recherche_courante_news = JSON.parse(cookie);
+    }
     var url = "search.php?data=" + encodeURI(recherche_courante);
     ajax_get_request(maj_resultats, url, true);
     $("#wait").css("display", "block");
@@ -65,33 +94,36 @@ function maj_resultats(res)
 {
     var parsed = JSON.parse(res);
     for(var i = 0; i < parsed.length; i++){
-        $("#resultats").append("<p class=\"titre_result\"><a class=\"titre_news\" href=\""
-        + decodeEntities(parsed[i]["url"])
-        + "\" target=\"_blank\">"
-        + decodeEntities(parsed[i]["titre"])
-        + "</a><span class=\"date_news\">"
-        + format(decodeEntities(parsed[i]["date"]))
-        + "</span><span class=\"action_news\" onclick=\"sauver_nouvelle(this)\"><img src=\"horloge15.jpg\"/></span></p>");
+        afficher_resultat(parsed[i], indexOf(recherche_courante_news, make_nouvelle_object(parsed[i])) >= 0);
     }
     
     $("#wait").css("display", "none");
 }
 
+function make_nouvelle_object(e){
+    var url = $(e).parent().find(".titre_news").attr("href");
+    var titre = $(e).parent().find(".titre_news").val();
+    var date = $(e).parent().find(".date_news").val();
+    return {"url":url, "titre":titre, "date":date};
+
+}
 
 function sauver_nouvelle(e)
 {
     $(e).find("img").attr("src","disk15.jpg");
     $(e).attr("onclick", "supprimer_nouvelle(this)");
-    var url = $(e).parent().find(".titre_news").attr("href");
-    var titre = $(e).parent().find(".titre_news").val();
-    var date = $(e).parent().find(".date_news").val();
-    var objet_resultat = {"url":url, "titre":titre, "date":date};
-    recherche_courante_news.push(objet_resultat);
+    var objet_resultat = make_nouvelle_object();
+    if(indexOf(recherche_courante_news, objet_resultat) < 0)
+        recherche_courante_news.push(objet_resultat);
 }
-
 
 function supprimer_nouvelle(e)
 {
     $(e).find("img").attr("src","horloge15.jpg");
     $(e).attr("onclick", "sauver_nouvelle(this)");
+    var objet_resultat = make_nouvelle_object();
+    var index = indexOf(recherche_courante_news, objet_resultat);
+    if(index >= 0)
+        recherche_courante_news.splice(index, 1);
+    $.cookie(recherche_courante, JSON.stringify(recherche_courante_news), {expires : 1000});
 }
