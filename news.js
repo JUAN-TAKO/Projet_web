29e40348ad468cb8b82872d2d128ed351e8742d3
JUAN TAKO <juan.tako.dev@gmail.com>
2018-10-14 23:33:06 +0200

    test

--- news.js
+++ news.js
@@ -97,6 +97,7 @@ function maj_resultats(res)
     var parsed = JSON.parse(res);
     console.log(parsed[0]);
     for(var i = 0; i < parsed.length; i++){
+        parsed[i]["date"] = format(parsed[i]["date"]);
         afficher_resultat(parsed[i], indexOf(recherche_courante_news, parsed[i]) >= 0);
     }
     
